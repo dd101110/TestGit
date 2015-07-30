f02d614dccdda8437f321c9255b267e2f71ef6bc
yibin6 <yibin@jd.com>
2015-07-30 16:21:23 +0800

    branch test

--- app/src/main/java/com/example/yibin/testgit/FirstActivity.java
+++ app/src/main/java/com/example/yibin/testgit/FirstActivity.java
@@ -10,5 +10,6 @@ public class FirstActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Log.d
     }
 }

d64ec4598989f108374374da2369249f62a20106
yibin6 <yibin@jd.com>
2015-07-30 16:10:11 +0800

    add FirstActivity.java

---
+++ app/src/main/java/com/example/yibin/testgit/FirstActivity.java
@@ -0,0 +1,14 @@
+package com.example.yibin.testgit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/7/30.
+ */
+public class FirstActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
